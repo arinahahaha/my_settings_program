b3228ae236ce2e6068a786d82d6fee94637fb4d8
arinahahaha <haritonova2507@icloud.com>
2024-06-05 17:22:54 +0300

    добавлена функция вывода параметров структуры человека

---
+++ Project24/printPars.h
@@ -0,0 +1,9 @@
+#include <string>
+#include "rstruct_person.h"
+
+void printPersonPars(Person p)
+{
+	cout << "װָ־: " << p.name << p.secondname << p.surname << "." <<endl;
+	cout << "גמחנאסע: " << p.age << endl;
+
+}
